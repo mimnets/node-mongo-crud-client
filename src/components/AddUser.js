b3525773a8d16e71fef5f4bd8eec130704d6f25e
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-12-13 08:31:48 +0600

    send data to the server

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -7,6 +7,17 @@ const AddUser = () => {
     const handleAddUser = (event) => {
         event.preventDefault();
         console.log(user);
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
     }
 
     const handleInputBlur = (event) => {
