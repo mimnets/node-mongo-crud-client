950c0e0213f0853ac539467b38d27d35fbbe8a0a
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-12-13 20:11:18 +0600

    delete user from db

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
     const users = useLoaderData()
-
+    const [displayUsers, setDisplayUsers] = useState(users)
+    
     const handleDelete = user =>{
         const agree = window.confirm('Are you sure you want to delete');
         if(agree){
-            console.log('Deleting user ' + user._id)
+            // console.log('Deleting user ' + user._id)
+            fetch(`http://localhost:5000/users/${user._id}`, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.deletedCount > 0) {
+                    alert('User deleted')
+                    const remainingUsers = displayUsers.filter(usr => usr._id !== user._id);
+                    setDisplayUsers(remainingUsers);
+                }
+            });
         }
     }
     return (
